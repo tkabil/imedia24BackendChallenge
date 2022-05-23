39716fa63b759f5b546685b5b0d43de667179f7c
tkabil <tahayassine.kabil@sicpa.com>
2022-05-23 01:20:38 +0100

    first Commit add Backend part

---
+++ src/main/kotlin/ma/imedia24/demo/config/SwaggerConfiguration.kt
@@ -0,0 +1,20 @@
+package ma.imedia24.demo.config
+import org.springframework.context.annotation.Bean
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableSwagger2
+internal class SwaggerConfiguration {
+    @Bean
+    open fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+}
