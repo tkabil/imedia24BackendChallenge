7328f352ab8b063cc9731452315a72a1db09fc55
tkabil <tahayassine.kabil@sicpa.com>
2022-05-23 02:26:15 +0100

    FIX swagger configuration

--- src/main/kotlin/ma/imedia24/demo/DemoApplication.kt
+++ src/main/kotlin/ma/imedia24/demo/DemoApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class DemoApplication
-
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
--- src/main/kotlin/ma/imedia24/demo/config/SwaggerConfiguration.kt
+++ src/main/kotlin/ma/imedia24/demo/config/SwaggerConfiguration.kt
@@ -1,20 +1,46 @@
 package ma.imedia24.demo.config
+
 import org.springframework.context.annotation.Bean
-import springfox.documentation.swagger2.annotations.EnableSwagger2
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 
-import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableSwagger2
+@EnableWebMvc
 internal class SwaggerConfiguration {
     @Bean
-    open fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
-            .build()
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ma.imedia24.demo"))
+                .paths(PathSelectors.any())
+                .build()
+    }
+
+    private fun getApiInfo(): ApiInfo {
+        val contact = Contact("API BACKEND ", "", "ty.kabil@gmail.com")
+        return ApiInfoBuilder()
+                .title("API BACKEND FOR PRODUCT AND CATEGORIES")
+                .description("API BACKEND FOR PRODUCT AND CATEGORIES")
+                .version("1.0.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .contact(contact)
+                .build()
+    }
+    fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/")
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
 }
